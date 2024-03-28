23af45cc0ca1742fc0831190820c3c43dbcd22c2
yoni-j <yonyjacoby@gmail.com>
2024-03-28 23:31:01 +0200

    add asyncio

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import os
 import telegram
 import functions_framework
 import logging
+import asyncio
 
 logger = logging.getLogger()
 
@@ -12,17 +13,11 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 @functions_framework.http
 def handle_update(request):
     update = telegram.Update.de_json(request.get_json(), bot=telegram.Bot(token=BOT_TOKEN))
-    logger.info("started!!!!!!!")
-
-    # Extract the user message
     message = update.message.text
-    logger.info(message)
 
-    # Handle the message (basic echo example)
     if message:
         reply_text = f"You said: {message}"
-        logger.info(reply_text)
         bot = telegram.Bot(token=BOT_TOKEN)
-        bot.send_message(chat_id=update.message.chat_id, text=reply_text)
+        asyncio.run(bot.send_message(chat_id=update.message.chat_id, text=reply_text))
 
     return "OK"
