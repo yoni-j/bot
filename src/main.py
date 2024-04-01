f0e13c5300e6e90d55b5634c816086e40623a9f5
yoni-j <yonyjacoby@gmail.com>
2024-04-01 22:12:29 +0300

    fix

--- src/main.py
+++ src/main.py
@@ -20,29 +20,29 @@ topic_name = "llm-topic"
 @functions_framework.http
 def handle_update(request):
     bot = telegram.Bot(token=BOT_TOKEN)
-    print(request.get_json())
-    update = telegram.Update.de_json(request.get_json(), bot=bot)
-    message = update.message.text
+    request_body = request.get_json()
 
-    if message:
-        # Handle message from llm service
-        if LLM_MESSAGE_PREFIX in message:
-            message = message.replace(LLM_MESSAGE_PREFIX, '')
-            asyncio.run(bot.send_message(chat_id=update.message.chat_id, text=message))
-            return "OK"
+    # Handle llm message
+    if "chat_id" in request_body:
+        text = request_body["text"]
+        message = text.replace(LLM_MESSAGE_PREFIX, '')
+        asyncio.run(bot.send_message(chat_id=request_body["chat_id"], text=message))
+        return "OK"
 
-        # Handle user message
-        else:
-            publisher = pubsub_v1.PublisherClient()
-            topic_path = publisher.topic_path(project_id, topic_name)
-            message_bytes = json.dumps(
-                {"chat_id": update.message.chat_id,
-                 "message": message}
-            ).encode("utf-8")
-            try:
-                publish_future = publisher.publish(topic_path, data=message_bytes)
-                publish_future.result()
-            except Exception as e:
-                print(f"Error publishing message: {e}")
+    # Handle user message
+    else:
+        update = telegram.Update.de_json(request_body, bot=bot)
+        message = update.message.text
+        publisher = pubsub_v1.PublisherClient()
+        topic_path = publisher.topic_path(project_id, topic_name)
+        message_bytes = json.dumps(
+            {"chat_id": update.message.chat_id,
+             "message": message}
+        ).encode("utf-8")
+        try:
+            publish_future = publisher.publish(topic_path, data=message_bytes)
+            publish_future.result()
+        except Exception as e:
+            print(f"Error publishing message: {e}")
 
     return "OK"
