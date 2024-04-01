afb6b99eb5fa8135fa06a5f3820b81fdccca0e5f
yoni-j <yonyjacoby@gmail.com>
2024-04-01 21:55:19 +0300

    connect llm

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from google.cloud import pubsub_v1
 logger = logging.getLogger()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+LLM_MESSAGE_PREFIX = "#from_llm#"
 
 project_id = "yonidev"
 topic_name = "llm-topic"
@@ -22,16 +23,23 @@ def handle_update(request):
     message = update.message.text
 
     if message:
-        reply_text = f"You said: {message}"
-        bot = telegram.Bot(token=BOT_TOKEN)
-        publisher = pubsub_v1.PublisherClient()
-        topic_path = publisher.topic_path(project_id, topic_name)
-        message_bytes = reply_text.encode("utf-8")
-        try:
-            publish_future = publisher.publish(topic_path, data=message_bytes)
-            publish_future.result()
-        except Exception as e:
-            print(f"Error publishing message: {e}")
-        asyncio.run(bot.send_message(chat_id=update.message.chat_id, text=reply_text))
+        if LLM_MESSAGE_PREFIX in message:
+            message = message.replace(LLM_MESSAGE_PREFIX, '')
+            bot = telegram.Bot(token=BOT_TOKEN)
+            asyncio.run(bot.send_message(chat_id=update.message.chat_id, text=message))
+            return "OK"
+
+        else:
+            publisher = pubsub_v1.PublisherClient()
+            topic_path = publisher.topic_path(project_id, topic_name)
+            message_bytes = json.dumps(
+                {"chat_id": update.message.chat_id,
+                 "message": message}
+            ).encode("utf-8")
+            try:
+                publish_future = publisher.publish(topic_path, data=message_bytes)
+                publish_future.result()
+            except Exception as e:
+                print(f"Error publishing message: {e}")
 
     return "OK"
